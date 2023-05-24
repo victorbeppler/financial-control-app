c9c4a0cfdfbc91872706533786d1b3384c0fea9c
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-23 21:21:50 -0300

    Build(create file future page register)

---
+++ src/page/register/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function Register() {
+  return (
+    <>
+      <h1>Register</h1>
+    </>
+  );
+}
+
+export default Register;
