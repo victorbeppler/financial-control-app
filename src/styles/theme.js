15acc9dffb8bfae2af0b8a260549da81914fc66a
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-23 21:22:43 -0300

    Build(config theme global)

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    primary: "#d22042",
+    background: "#000",
+  },
+  fontSizes: {
+    small: "14px",
+    medium: "16px",
+    large: "18px",
+  },
+  // ... outras propriedades do tema
+};
+
+export default theme;
