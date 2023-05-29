be24ae35d6b1a146c21161ddeee30263eb62d1d4
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-29 19:52:33 -0300

    build(add new route in app)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./page/home";
+import Environment from "./page/environment";
 import Login from "./page/login";
 import Register from "./page/register";
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Environment />} />
+        <Route path="/environment/:idEnvironment" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         {/* <Route path="/register" element={<Form />} /> */}
