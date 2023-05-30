b6ec9f56e08567dcdc87855a92038ca7c826aff6
Victor Hugo <victorhugobeppler@gmail.com>
2023-05-30 12:53:02 -0300

    Build(comment line)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Container } from "./styles";
 
 function Header() {
-  const user = JSON.parse(localStorage.getItem("user"));
+  // const user = JSON.parse(localStorage.getItem("user"));
   return <Container>{/* <h1>{user.name}</h1> */}</Container>;
 }
 
