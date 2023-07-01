8df7733c6e114eb79075dd4aca3f7fd94726b67c
Victor Hugo <victorhugobeppler@gmail.com>
2023-07-01 00:34:59 -0300

    Build(add dns in Api base back)

--- src/services/base-back.js
+++ src/services/base-back.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ApiBack = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://back.expense.victorbeppler.dev/api",
 });
 
 export default ApiBack;
