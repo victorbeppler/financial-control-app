8ce0867d7fecbb885100fa16074a494ff7bb52f7
Victor Hugo <victorhugobeppler@gmail.com>
2023-06-30 22:57:51 -0300

    Feat(create feat in Header) - add button singout - add button send home page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 
-import { Container } from "./styles";
+import {
+  ButtonHome,
+  ButtonSingOut,
+  Container,
+  ContainerHome,
+  ContainerSingOut,
+  Icon,
+} from "./styles";
 
 function Header() {
-  // const user = JSON.parse(localStorage.getItem("user"));
-  return <Container>{/* <h1>{user.name}</h1> */}</Container>;
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  function handleSingOut() {
+    localStorage.removeItem("user");
+    window.location.reload();
+  }
+
+  function handleHome() {
+    window.location.href = "/";
+  }
+
+  return (
+    <Container>
+      {/* {<h1>{user.name}</h1>} */}
+      <ContainerHome>
+        <ButtonHome onClick={handleHome}>
+          <Icon src="/house-bold.png" />
+        </ButtonHome>
+      </ContainerHome>
+      <ContainerSingOut>
+        <ButtonSingOut onClick={handleSingOut}>
+          <Icon src="/sign-out-bold.png" />
+        </ButtonSingOut>
+      </ContainerSingOut>
+    </Container>
+  );
 }
 
 export default Header;
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -13,3 +13,41 @@ export const Container = styled.div`
   border-bottom-left-radius: 10px;
   border-bottom-right-radius: 10px;
 `;
+
+export const Icon = styled.img`
+  width: 30px;
+  height: 30px;
+  /* margin-left: 10px; */
+`;
+
+export const ContainerHome = styled.div`
+  width: 90%;
+  display: flex;
+  padding-left: 50px;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const ContainerSingOut = styled.div`
+  width: 10%;
+  padding-right: 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const ButtonSingOut = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  border: none;
+`;
+
+export const ButtonHome = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  border: none;
+`;
