af95240868f2779387241f8b72cd730a18b3b691
Victor Hugo <victorhugobeppler@gmail.com>
2023-07-11 21:26:15 -0300

    Feat(increase more px in border radius header)

--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -10,8 +10,8 @@ export const Container = styled.div`
   align-items: center;
   background-color: ${(props) => props.theme.colors.primary};
   box-shadow: 0px 0px 8px #000;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
+  border-bottom-left-radius: 60px;
+  border-bottom-right-radius: 60px;
 `;
 
 export const Icon = styled.img`
